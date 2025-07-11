b0c55da5b67bac313de18761c5a771ad1e89c00e
datblue21 <92386475+datblue21@users.noreply.github.com>
2025-07-11 09:35:38 +0700

    commit day 10

---
+++ Day10/WebProject/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import TasksManagement from './TasksManagement'
+
+function App() {
+  
+
+  return (
+    <React.Fragment>
+      <TasksManagement />
+    </React.Fragment>
+  )
+}
+
+export default App
